78e39bf14bf5d1032205fa9722969d1918065d21
AhmadZaydan06 <ahmadzaydan921@gmail.com>
2026-05-05 22:36:30 +0700

    tugas backend 1

--- pages/anggota_hapus.php
+++ pages/anggota_hapus.php
@@ -1,20 +1,27 @@
 <?php
-include '../config/koneksi.php';
 session_start();
-if ($_SESSION['role'] != 'Admin') die("Akses Ditolak!");
+include 'config/koneksi.php';
 
-$id   = $_GET['id'];
-$type = $_GET['type'];
-
-if ($type == 'soft') {
-    mysqli_query($conn, "UPDATE anggota SET deleted_at = NOW() WHERE id_anggota = $id");
-} elseif ($type == 'restore') {
-    // Balikin data dengan menset NULL pada deleted_at
-    mysqli_query($conn, "UPDATE anggota SET deleted_at = NULL WHERE id_anggota = $id");
-} else {
-    // Hard Delete
-    mysqli_query($conn, "DELETE FROM anggota WHERE id_anggota = $id");
+if (!isset($_SESSION['role']) || $_SESSION['role'] == 'viewer') {
+    header("Location: login.php?pesan=restricted");
+    exit();
 }
 
-header("Location: anggota_tampil.php");
+if (isset($_GET['id'])) {
+    $id_hapus = $_GET['id'];
+
+    $sql = "DELETE FROM anggota WHERE id_anggota = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id_hapus);
+
+    if ($stmt->execute()) {
+        header("Location: anggota_tampil.php?status=deleted");
+        exit();
+    } else {
+        echo "Gagal menghapus data: " . $conn->error;
+    }
+} else {
+    header("Location: anggota_tampil.php");
+    exit();
+}
 ?>
